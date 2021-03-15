80806c914c49024b84d906a834f28c7328a89f9e
alexgamadev <alexgama.97@outlook.com>
2021-03-15 23:11:22 +0000

    Added isvalidplacement function and tests

--- src/factories/GameboardFactory.test.ts
+++ src/factories/GameboardFactory.test.ts
@@ -1,8 +1,24 @@
-import createGameboard from './GameboardFactory';
+import createGameboard, { GameboardInterface } from './GameboardFactory';
 
 describe('Gameboard initialisation', () => {
     test('Gameboard initialises grid', () => {
         expect(createGameboard().grid.length).toBe(100);
         expect(createGameboard().grid[36]).toBe(-1);
+    });
+})
+
+describe('Gameboard checkValidPlacement', () => {
+    let gameboard: GameboardInterface;
+
+    beforeEach(() => {
+        gameboard = createGameboard();
+    });
+
+    test('Check single coordinate is valid', () => {
+        expect(gameboard.isValidPlacement([8, 3])).toBe(true);
+    });
+
+    test('Check single coordinate is invalid', () => {
+        expect(gameboard.isValidPlacement([-1, 8])).toBe(false);
     });
 })
--- src/factories/GameboardFactory.ts
+++ src/factories/GameboardFactory.ts
@@ -1,10 +1,24 @@
-interface Gameboard {
+
+export interface GameboardInterface {
     grid: number[],
+    isValidPlacement: (coordinate: [number, number]) => boolean,
 }
 
-export default function createGameboard(): Gameboard {
+export default function createGameboard(): GameboardInterface {
     const grid: number[] = new Array(100);
     grid.fill(-1);
 
-    return {grid};
+    function isValidPlacement (coordinate: [number, number]) : boolean {
+        if(coordinate[0] < 0 || coordinate[1] < 0) return false;
+        if(coordinate[0] >= 10 || coordinate[1] >= 10) return false;
+        
+        return true;
+    }
+
+    const gameboard = {
+        grid,
+        isValidPlacement,
+    }
+
+    return gameboard;
 }
