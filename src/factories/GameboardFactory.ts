5d622e9b036358a413e7a94087ad28465c54f74d
alexgamadev <alexgama.97@outlook.com>
2021-03-15 22:27:01 +0000

    Added return type for factory function

--- src/factories/GameboardFactory.ts
+++ src/factories/GameboardFactory.ts
@@ -2,8 +2,8 @@ interface Gameboard {
     grid: number[],
 }
 
-export default function createGameboard() {
-    const grid = new Array(100);
+export default function createGameboard(): Gameboard {
+    const grid: number[] = new Array(100);
     grid.fill(-1);
 
     return {grid};
