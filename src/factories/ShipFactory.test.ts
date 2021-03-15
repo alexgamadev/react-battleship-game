d3580e8b3a6a0604096dd1d07f568909938b3cfb
alexgamadev <alexgama.97@outlook.com>
2021-03-15 22:10:09 +0000

    Implemented core ship functionality with tests

--- src/factories/ShipFactory.test.ts
+++ src/factories/ShipFactory.test.ts
@@ -1,16 +1,61 @@
-import ShipFactory, {ShipInterface} from './ShipFactory';
+import createShip, {ShipTypes} from './ShipFactory';
 
-describe('Ship Factory', () => {
-    it('Returns correct object', () => {
-        const expectedShip: ShipInterface = {
-            length: 3,
-            partsHit: [false, false, false],
-            isSunk: false,
-        }
-        expect(ShipFactory(3)).toStrictEqual(expectedShip);
+describe('Ship initialisation', () => {
+    it('Returns null if length invalid', () => {
+        expect(createShip(-1)).toBe(null);
+        expect(createShip(ShipTypes.LENGTH_LIMIT)).toBe(null);
     });
 
-    it('Returns null if length invalid', () => {
-        expect(ShipFactory(-1)).toBe(null);
+    it('Returns correct length', () => {
+        expect(createShip(ShipTypes.SUBMARINE)?.length).toBe(3);
+    });
+
+    test('Initial parts hit are all false', () => {
+        expect(createShip(ShipTypes.SUBMARINE)?.partsHit).toStrictEqual([false, false, false]);
+    });
+
+    test('Ship is sunk by default', () => {
+        expect(createShip(ShipTypes.SUBMARINE)?.isSunk()).toBe(false);
     });
 })
+
+describe('Ship hit function', () => {
+    let ship: any;
+
+    beforeEach(() => {
+        ship = createShip(ShipTypes.SUBMARINE);
+    });
+
+    test('Hit returns true when valid', () => {
+        expect(ship?.hit(2)).toBe(true);
+    })
+
+    test('Invalid hit location returns null', () => {
+        expect(ship?.hit(3)).toBe(false);
+        expect(ship?.partsHit).toStrictEqual([false, false, false]);
+    });
+
+    test('Parts can be hit', () => {
+        ship?.hit(2);
+        expect(ship?.partsHit).toStrictEqual([false, false, true]);
+    });
+
+    test('Multiple parts can be hit', () => {
+        ship?.hit(2);
+        ship?.hit(0);
+        expect(ship?.partsHit).toStrictEqual([true, false, true]);
+    });
+
+    test('Same parts cannot be hit twice', () => {
+        ship?.hit(2);
+        expect(ship?.hit(2)).toBe(false);
+    });
+
+    test('Ship can be sunk', () => {
+        ship?.hit(0);
+        ship?.hit(1);
+        ship?.hit(2);
+        expect(ship?.partsHit).toStrictEqual([true, true, true]);
+        expect(ship?.isSunk()).toBe(true);
+    })
+});
--- src/factories/ShipFactory.ts
+++ src/factories/ShipFactory.ts
@@ -1,20 +1,51 @@
 export interface ShipInterface {
     length: number,
     partsHit: boolean[],
-    isSunk: boolean,
+    isSunk: () => boolean,
+    hit: (location: number) => boolean,
+}
+
+export enum ShipTypes {
+    SPEED_BOAT = 1,
+    PATROL_BOAT,
+    SUBMARINE,
+    BATTLESHIP,
+    CARRIER,
+    LENGTH_LIMIT,
 }
 
 
-export default function ShipFactory(length : number) : ShipInterface | null {
+export default function createShip(length : number) : ShipInterface | null {
     if(length <= 0) return null;
-    
+    if(length > ShipTypes.LENGTH_LIMIT - 1) return null;
+
+    //Fill parts hit array with initial values
     const partsHit = new Array(length);
     partsHit.fill(false);
 
+    function hit (location : number) : boolean  {
+        //Ensure hits only happen within ship boundaries
+        if(location > partsHit.length - 1 || location < 0) return false;
+        //Ensure same part can't be hit twice
+        if(partsHit[location]) return false;
+
+        partsHit[location] = true;
+        return true;
+    }
+
+    function isSunk() : boolean {
+        //If every part has been hit, ship is sunk
+        if(partsHit.every(part => part === true)) return true;
+        return false;
+    }
+
+    //Create ship object
     const ship : ShipInterface = {
         length,
         partsHit,
-        isSunk: false,
+        isSunk,
+        hit, 
     }
+
     return ship;
 }
