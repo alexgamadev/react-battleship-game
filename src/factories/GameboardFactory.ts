223b18ebe618b926cb6588168afed7e2b915e4a6
alexgamadev <alexgama.97@outlook.com>
2021-03-16 10:06:34 +0000

    Implemented horizontal, multiple length placement checking

--- src/factories/GameboardFactory.ts
+++ src/factories/GameboardFactory.ts
@@ -1,17 +1,30 @@
+import {direction, coordinate, dimensions} from '../types';
 
 export interface GameboardInterface {
     grid: number[],
-    isValidPlacement: (coordinate: [number, number]) => boolean,
+    isValidPlacement: (coordinates: coordinate, dimensions?: dimensions) => boolean,
+}
+
+function isValidCoordinate(coordinates: coordinate) : boolean {
+    if(coordinates[0] < 0 || coordinates[1] < 0) return false;
+    if(coordinates[0] >= 10 || coordinates[1] >= 10) return false;
+
+    return true;
 }
 
 export default function createGameboard(): GameboardInterface {
     const grid: number[] = new Array(100);
     grid.fill(-1);
 
-    function isValidPlacement (coordinate: [number, number]) : boolean {
-        if(coordinate[0] < 0 || coordinate[1] < 0) return false;
-        if(coordinate[0] >= 10 || coordinate[1] >= 10) return false;
-        
+    function isValidPlacement (coordinates: coordinate, dimensions?: dimensions) : boolean {
+        if(!isValidCoordinate(coordinates)) return false;
+        if(!dimensions) {
+            return true;
+        }
+
+        const endPos: coordinate = [coordinates[0] + dimensions[0], coordinates[1]];
+        if(!isValidCoordinate(endPos)) return false;
+
         return true;
     }
 
